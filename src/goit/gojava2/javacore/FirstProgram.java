0a6b16ecdad59dd866662214fc20e457d21be8b5
Copypaster <khruschiov@gmail.com>
2016-02-28 09:59:52 +0200

    first commit

---
+++ src/goit/gojava2/javacore/FirstProgram.java
@@ -0,0 +1,7 @@
+package goit.gojava2.javacore;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your first Java program! Stay tuned!");
+    }
+}
