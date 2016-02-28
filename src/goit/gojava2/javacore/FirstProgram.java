42f3a3c3de0bda1942e392f341ce4c6823244e09
Copypaster <khruschiov@gmail.com>
2016-02-28 11:10:30 +0200

    undo last changes

--- src/goit/gojava2/javacore/FirstProgram.java
+++ src/goit/gojava2/javacore/FirstProgram.java
@@ -3,7 +3,5 @@ package goit.gojava2.javacore;
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hi, I'm your first Java program! Stay tuned!");
-        System.out.println("Changes");
-
     }
 }
