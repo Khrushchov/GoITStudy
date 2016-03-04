5426e0fb01d416d1a6d94ad60df6d5cc2ff6484f
Copypaster <khruschiov@gmail.com>
2016-03-04 14:16:09 +0200

    add module1

--- src/goit/gojava2/javacore/module1/FirstProgram.java
+++ src/goit/gojava2/javacore/module1/FirstProgram.java
@@ -1,6 +1,6 @@
-package goit.gojava2.javacore;
+package goit.gojava2.javacore.module1;
 
-public class FirstProgram {
+ class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hi, I'm your first Java program! Stay tuned!");
     }
